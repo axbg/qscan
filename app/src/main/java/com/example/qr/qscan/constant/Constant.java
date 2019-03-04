4eb3ed064ead2bc7ecb0b3e65d73ea9420bc133c
Alex Bișag <bisagalexstefan@gmail.com>
2019-03-04 23:04:18 +0200

    Removed endpoints.

--- app/src/main/java/com/example/qr/qscan/constant/Constant.java
+++ app/src/main/java/com/example/qr/qscan/constant/Constant.java
@@ -4,9 +4,9 @@ public interface Constant {
 
     String SHARED_PREFERENCES = "SHARED_PREFERENCES";
     String TOKEN_PREFERENCE = "TOKEN_PREFERENCE";
-    String BASE_URL = "https://scanit.sisc.ro";
-    String BASE_URL_REST = "https://scanit.sisc.ro/check-in/rest/qr";
-    String LOGIN_URL = BASE_URL + "/login/rest";
+    String BASE_URL = "";
+    String BASE_URL_REST = "";
+    String LOGIN_URL = BASE_URL + "";
     String CREDENTIALS = "CREDENTIALS";
     String GET = "GET_METHOD";
     String POST = "POST_METHOD";
